8bd154f72d4061ccb4d45fa81e09b8330ea8de5c
Jors Academy <abalci16@posta.pau.edu.tr>
2026-08-30 12:21:00 +0300

    Export predictive optimization components

--- src/continuous_reoptimization/__init__.py
+++ src/continuous_reoptimization/__init__.py
@@ -3,15 +3,19 @@ from .events import EdgeClosed, EdgeCostChanged, EdgeOpened, ManualReoptimize, T
 from .model import OptimizationState, RouteSolution
 from .optimizer import DijkstraOptimizer, IncrementalShortestPathOptimizer
 from .policy import TriggerPolicy
+from .predictive import PredictiveDijkstraOptimizer
+from .predictor import EdgeCostPredictor
 
 __all__ = [
     "DijkstraOptimizer",
     "EdgeClosed",
     "EdgeCostChanged",
+    "EdgeCostPredictor",
     "EdgeOpened",
     "IncrementalShortestPathOptimizer",
     "ManualReoptimize",
     "OptimizationState",
+    "PredictiveDijkstraOptimizer",
     "ReoptimizationEngine",
     "RouteSolution",
     "TimerExpired",
